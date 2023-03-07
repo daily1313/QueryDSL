d8f6a2dad600b6d3262adac0da35d32b20a153f6
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-08 02:22:30 +0900

    Initial commit

---
+++ src/test/java/study/QueryDSL/QueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.QueryDSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
