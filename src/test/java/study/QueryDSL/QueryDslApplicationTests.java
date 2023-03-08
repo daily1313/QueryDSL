013e5a1c460ee1aa23ce983b7304559500e80fb0
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-08 22:15:22 +0900

    feat : querydsl 설정과 검증로직 추가

--- src/test/java/study/QueryDSL/QueryDslApplicationTests.java
+++ src/test/java/study/QueryDSL/QueryDslApplicationTests.java
@@ -1,13 +1,36 @@
 package study.QueryDSL;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.QueryDSL.entity.Hello;
+import study.QueryDSL.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QueryDslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
