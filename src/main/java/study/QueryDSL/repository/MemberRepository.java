0861ffca1421b4940e0b4d5c47d0b83f92006995
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-12 20:00:47 +0900

    feat : JPA에서 제공하는 QuerydslPredicateExecutor 인터페이스 기능 적용

--- src/main/java/study/QueryDSL/repository/MemberRepository.java
+++ src/main/java/study/QueryDSL/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package study.QueryDSL.repository;
 
 import java.util.List;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.QueryDSL.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom,
+        QuerydslPredicateExecutor<Member> {
     // select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
 }
--- src/test/java/study/QueryDSL/repository/MemberRepositoryTest.java
+++ src/test/java/study/QueryDSL/repository/MemberRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import java.util.List;
 import javax.persistence.EntityManager;
+import org.apache.catalina.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.QueryDSL.dto.MemberSearchCondition;
 import study.QueryDSL.dto.MemberTeamDto;
 import study.QueryDSL.entity.Member;
+import study.QueryDSL.entity.QMember;
 import study.QueryDSL.entity.Team;
 
 @SpringBootTest
@@ -99,6 +101,36 @@ class MemberRepositoryTest {
 
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1","member2","member3");
+    }
+    
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1",10, teamA);
+        Member member2 = new Member("member2",20, teamA);
+
+        Member member3 = new Member("member3",30, teamB);
+        Member member4 = new Member("member4",40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("member1 = " + findMember);
+        }
+            
 
     }
+    
 }
