8698ec4fb5116f1ddd3848c6ee147fa2a1e71c78
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-03-12 18:37:59 +0900

    feat : 조회용 샘플 데이터 및 조회 API 추가

---
+++ src/main/java/study/QueryDSL/controller/MemberController.java
@@ -0,0 +1,22 @@
+package study.QueryDSL.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.QueryDSL.dto.MemberSearchCondition;
+import study.QueryDSL.dto.MemberTeamDto;
+import study.QueryDSL.repository.MemberJpaRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+
+}
